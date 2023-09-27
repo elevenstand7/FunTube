9204864970ce5a8bde304c0801428b8b96cc3599
Yinyin <elevenstand7@gmail.com>
2023-09-27 11:43:36 -0400

    modify comment input box and button

--- frontend/src/components/CreateCommentForm/index.js
+++ frontend/src/components/CreateCommentForm/index.js
@@ -1,7 +1,21 @@
-
-
+import React from 'react';
+import { NavLink, useHistory, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import button from 'bootstrap';
+import "./CreateCommentForm.css"
 
 const CreateCommentForm = ()=>{
+    const dispatch = useDispatch();
+    const {commentId} = useParams();
+
+    return (
+        <div>
+            <div className="user-input-container">
+                <textarea id='user-input' placeholder='Add a comment...'></textarea>
+                <button className='btn comment-btn'>Comment</button>
+            </div>
+        </div>
+    )
 
 }
 
