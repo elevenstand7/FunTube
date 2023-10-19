6965d0c216dd43a50177e9bfd20e286dee197e07
elevenstand7 <elevenstand7@gmail.com>
2023-10-19 11:41:53 -0400

    add about modal

--- frontend/src/components/MenuBar/index.js
+++ frontend/src/components/MenuBar/index.js
@@ -4,15 +4,22 @@ import {button,  Navbar, Nav, NavItem} from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import logo from './logo.png'
 import './MenuBar.css'
-// import bootstrap from 'bootstrap' ;
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Modal from 'react-bootstrap/Modal';
 
 
 function MenuBar() {
     const sessionUser = useSelector(state => state.session.user);
-    // const history = useHistory();
-    // const changeRoute = ()=>{
-    //     history.push(`/`);
-    //   }
+    const [show, setShow] = useState(false);
+
+    const handleOpenModal = ()=>{
+        setShow(true);
+    }
+
+    const handleCloseModal = ()=>{
+        setShow(false);
+    }
 
     return (
         <div className='menu-bar'>
@@ -20,7 +27,7 @@ function MenuBar() {
                 <button type="button" className='btn menu-btn'data-bs-toggle="offcanvas" data-bs-target="#side-bar">
                     <i className="fa-solid fa-bars"></i>
                 </button>
-                <div className="offcanvas offcanvas-start" tabIndex="-1" id="side-bar">
+                <div className="offcanvas offcanvas-start " tabIndex="-1" id="side-bar" >
                     <div className="offcanvas-header">
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
                     </div>
@@ -30,10 +37,50 @@ function MenuBar() {
                                 <i className="fa-solid fa-house side-bar-home-img"></i>
                                 <span>Home</span>
                             </button>
-
                         </a>
+                        <a className="side-bar-about" >
+                            <button className="side-bar-about-btn" onClick={handleOpenModal}>
+                                <i className="fa-solid fa-circle-question side-bar-about-img"></i>
+                                <span>About</span>
+                            </button>
+                            <Modal show={show} onHide={handleCloseModal} backdrop="static" keyboard={false} >
+                                <Modal.Header closeButton>
+                                    <Modal.Title className="about-text">Welcome to FunTube!</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <div className="about-text">
+                                        FunTube is a YouTube clone project, which is a video-sharing platform that allows users to discover
+                                        and watch different kinds of videos covering different categories.
+                                    </div>
+                                    <br />
+                                    <div className="about-text">
+                                        <h5>Explore Current Features</h5>
+                                        <li>Engage with content: Leave your comments, show appreciation through likes, and find all your favorite videos on your personalized 'Favorites' page.</li>
+                                        <li>Personalized channels: Your channel showcases all your contributions, creating a unique space that represents your taste and interests.</li>
+                                    </div>
+                                    <br />
+                                    <div className="about-text">
+                                        <h5>Stay Tuned for What's Next</h5>
+                                        <li>Video management: Upload and meticulously edit your videos, giving you full control over your content.</li>
+                                        <li>Recommendations: Lists recommended videos on each video page, enhancing your browsing experience.</li>
+                                    </div>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleCloseModal}>
+                                        OK
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </a>
+
                     </div>
+
+
+
                 </div>
+
+
+
                 {/* {menuOpen? (<div className="menu">Open </div>) : null } */}
             </div>
             <div className='menu-logo'>
@@ -41,17 +88,10 @@ function MenuBar() {
                 <img className="page-logo"src={logo}></img>
             </NavLink>
             </div>
+
         </div>
 
-        // <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-        // activeKey="/"
-        // onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-        // >
-        //     <div className="sidebar-sticky"></div>
-        // <NavItem>
-        //     <Nav.Link href="/home">Active</Nav.Link>
-        // </NavItem>
-        // </Nav>
+
     )
 }
 
