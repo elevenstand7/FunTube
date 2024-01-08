6d836384fc5b4f5955a7b261822dab038279a318
elevenstand7 <elevenstand7@gmail.com>
2024-01-08 16:01:41 -0500

    move modal to the navi bar, cannot pop up, need to fix bugs

--- frontend/src/components/NaviBar/index.js
+++ frontend/src/components/NaviBar/index.js
@@ -4,17 +4,23 @@ import button from 'bootstrap';
 import { useSelector } from 'react-redux';
 import UserProfile from '../UserProfile';
 import './NaviBar.css';
+import VideoUploadModal from '../VideoUploadModal';
+import Modal from 'react-bootstrap/Modal';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function NaviBar() {
   const sessionUser = useSelector(state => state.session.user);
   const history = useHistory();
   const [uploadModal, setUploadModal] = useState(false);
+
   const changeRoute = ()=>{
     history.push(`/login`);
   }
 
   const openUploadModal = ()=>{
-
+    console.log("upload btn click!");
+    setUploadModal(true);
   }
   let sessionLinks;
   if (sessionUser) {
@@ -34,14 +40,33 @@ function NaviBar() {
           <i className="fa-solid fa-user"></i>
           <div className='btn-content'>LogIn</div>
         </button>
+        <Modal
+                show={uploadModal}
+                onHide={() => setUploadModal(false)}
+                className="upload-video-modal"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Upload videos</Modal.Title>
+                    {/* <button onClick={handleUploadVideo} className="btn upload-video-modal-btn">Upload</button> */}
+                </Modal.Header>
+                <Modal.Body>
+                    <div>Drag and drop video files to upload</div>
+                    <button className="btn">SELECT FILES</button>
+                </Modal.Body>
+            </Modal>
       </>
     );
   }
 
   return (
-    <div className='navi-btn'>
+    <>
+      <div className='navi-btn'>
         {sessionLinks}
-    </div>
+      </div>
+
+    </>
+
   );
 }
 
